a16c3e9ca1d00e3f1c56a23218126638e459d7b0
seus31 <us.balut.big.dipper@gmail.com>
2024-07-19 19:20:01 +0900

    add: app/bootstrapにapi用ルートを追加するための定義を追加

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,12 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        api: __DIR__.'/../routes/api.php',
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+//        $middleware->group('api', [
+//            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+//        ]);
+//
+//        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
