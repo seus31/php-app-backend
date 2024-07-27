9f8f10c8db2529136799437a3334f2122f46e39f
seus31 <us.balut.big.dipper@gmail.com>
2024-07-27 12:45:37 +0900

    add: NoteControllerに一覧用のメソッド追加
    (cherry picked from commit 6a74179a3dfbaf67d3e0c3e61dbc4b8f709841b6)

--- backend/app/Http/Controllers/Api/V1/NoteController.php
+++ backend/app/Http/Controllers/Api/V1/NoteController.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\NoteIndexRequest;
 use App\Http\Requests\Api\V1\NoteRequest;
 use App\Http\Resources\Api\V1\NoteResource;
+use App\Http\Resources\Api\V1\NoteResourceCollection;
 use App\Services\NoteService;
 
 class NoteController extends Controller
 {
-    protected $noteService;
+    protected NoteService $noteService;
 
     public function __construct(NoteService $noteService)
     {
         $this->noteService = $noteService;
     }
 
-    public function store(NoteRequest $request)
+    public function index(NoteIndexRequest $request): NoteResourceCollection
+    {
+        $notes = $this->noteService->getNotes($request->validated());
+        return new NoteResourceCollection($notes);
+    }
+
+    public function store(NoteRequest $request): NoteResource
     {
         $note = $this->noteService->createNote($request->validated());
         return new NoteResource($note);
