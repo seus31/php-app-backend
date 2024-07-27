38c2bdf9fea939f2cc5bd54abb5d4ed183d38908
seus31 <us.balut.big.dipper@gmail.com>
2024-07-27 12:47:34 +0900

    add: メモ一覧取得処理をRepositoryクラスに追加
    (cherry picked from commit 0f5d94eaef808b4c1ea030b4e9247fe3e8f65615)

--- backend/app/Repositories/NoteRepository.php
+++ backend/app/Repositories/NoteRepository.php
@@ -6,6 +6,11 @@ use App\Models\Note;
 
 class NoteRepository
 {
+    public function getNotes(array $noteIndexConditions)
+    {
+        return Note::paginate($noteIndexConditions['per_page'], ['*'], 'page', $noteIndexConditions['page']);
+    }
+
     public function create(array $note)
     {
         return Note::create($note);
