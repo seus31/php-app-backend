38c1f4d47f2818bbfc540410369551f77aa8973f
seus31 <us.balut.big.dipper@gmail.com>
2024-07-27 12:46:53 +0900

    add: ResourceCollectionクラスをオーバーライド

---
+++ backend/app/Http/Resources/Api/V1/ApiResourceCollection.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ApiResourceCollection extends ResourceCollection
+{
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'per_page' => $this->resource->perPage(),
+                'total' => $this->resource->total(),
+            ],
+        ];
+    }
+
+    public function additional($data)
+    {
+        return array_merge($data, [
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'per_page' => $this->resource->perPage(),
+                'total' => $this->resource->total(),
+            ],
+        ]);
+    }
+
+    protected function preparePaginatedData($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'per_page' => $this->resource->perPage(),
+                'total' => $this->resource->total(),
+            ],
+        ];
+    }
+
+    public function withResponse($request, $response)
+    {
+        $response->setData(array_merge(
+            $this->preparePaginatedData($request),
+            ['meta' => $this->meta()]
+        ));
+    }
+
+    protected function meta()
+    {
+        return [
+            'current_page' => $this->resource->currentPage(),
+            'last_page' => $this->resource->lastPage(),
+            'per_page' => $this->resource->perPage(),
+            'total' => $this->resource->total(),
+        ];
+    }
+}
