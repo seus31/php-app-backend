d2d578c68eb63a9d9debfe0ecf50fcf61b2e487a
seus31 <us.balut.big.dipper@gmail.com>
2024-08-29 12:00:00 +0900

    Requestのコメントを修正

--- backend/app/Http/Requests/Api/V1/CategoryIndexRequest.php
+++ backend/app/Http/Requests/Api/V1/CategoryIndexRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CategoryIndexRequest extends FormRequest
@@ -17,7 +18,7 @@ class CategoryIndexRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
