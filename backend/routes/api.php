fe522ad1cd79dcbdb19d31f63773ac7fb8a4e9d8
seus31 <us.balut.big.dipper@gmail.com>
2024-09-06 23:18:05 +0900

    ユーザー登録APIのルート追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\NoteController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    Route::post('register',  [AuthController::class, 'register']);
     Route::apiResource('notes', NoteController::class);
     Route::apiResource('categories', CategoryController::class);
 });
