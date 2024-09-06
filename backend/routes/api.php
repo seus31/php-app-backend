cebd5741f5e464e54fe7f5cb9900a13853eb1c4f
seus31 <us.balut.big.dipper@gmail.com>
2024-09-07 00:03:07 +0900

    ログイン・ログアウトのルート追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('register',  [AuthController::class, 'register']);
+    Route::post('login',  [AuthController::class, 'login']);
+    Route::post('logout',  [AuthController::class, 'logout']);
     Route::apiResource('notes', NoteController::class);
     Route::apiResource('categories', CategoryController::class);
 });
