3adbdfc61056c86b281f2cffb47f8c5f9f1d4c3b
seus31 <us.balut.big.dipper@gmail.com>
2024-09-07 00:03:20 +0900

    ログイン・ログアウトのAPI処理実装

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -31,5 +32,29 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json(['access_token' => $token, 'token_type' => 'Bearer'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+        }
+
+        return response()->json(['message' => 'The provided credentials are incorrect.'], 422);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
     }
 }
