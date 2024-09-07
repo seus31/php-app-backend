89b6f1125a11d2fe84b5826852e899a43934fb66
seus31 <us.balut.big.dipper@gmail.com>
2024-09-07 18:31:35 +0900

    フロント側にNotesのルート追加

--- frontend/src/layouts/Admin.js
+++ frontend/src/layouts/Admin.js
@@ -7,6 +7,7 @@ import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 import Dashboard from "views/admin/Dashboard.js";
+import Note from "views/admin/Note.js";
 
 export default function Admin() {
   return (
@@ -18,6 +19,7 @@ export default function Admin() {
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
+            <Route path="/admin/notes" exact component={Note} />
             <Redirect from="/admin" to="/admin/dashboard" />
           </Switch>
           <FooterAdmin />
