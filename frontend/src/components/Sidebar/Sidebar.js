f290b65dbd56cee932d930542a292b5f59fa0230
seus31 <us.balut.big.dipper@gmail.com>
2024-09-07 18:30:54 +0900

    Sidebarにnotesのリンク追加

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -95,6 +95,27 @@ export default function Sidebar() {
                   Dashboard
                 </Link>
               </li>
+              <li className="items-center">
+                <Link
+                  className={
+                    "text-xs uppercase py-3 font-bold block " +
+                    (window.location.href.indexOf("/admin/notes") !== -1
+                      ? "text-lightBlue-500 hover:text-lightBlue-600"
+                      : "text-blueGray-700 hover:text-blueGray-500")
+                  }
+                  to="/admin/notes"
+                >
+                  <i
+                    className={
+                      "fas fa-tv mr-2 text-sm " +
+                      (window.location.href.indexOf("/admin/notes") !== -1
+                        ? "opacity-75"
+                        : "text-blueGray-300")
+                    }
+                  ></i>{" "}
+                  Notes
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
