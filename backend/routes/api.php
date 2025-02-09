64d3d814df3a7c256f15269d9d5ac15cfe2481ce
seus31 <us.balut.big.dipper@gmail.com>
2025-02-10 00:53:23 +0900

    認証ルートをprefixにauthを追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('auth')->group(function () {
     Route::post('register',  [AuthController::class, 'register']);
     Route::post('login',  [AuthController::class, 'login']);
+});
+
+Route::prefix('v1')->group(function () {
     Route::post('logout',  [AuthController::class, 'logout']);
     Route::apiResource('notes', NoteController::class);
     Route::apiResource('categories', CategoryController::class);
